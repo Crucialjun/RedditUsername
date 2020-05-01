63937eae048ddbae52a0d06e394e1c03d44b3ffe
Crucialjun <nich.otieno@gmail.com>
2020-05-01 18:09:01 +0300

    Connect Fragment to activity

--- app/src/main/java/com/example/redditusername/MainActivity.kt
+++ app/src/main/java/com/example/redditusername/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.redditusername
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentManager = supportFragmentManager
+    private val detailsFragment = DetailsFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentsHolder,detailsFragment)
+        fragmentTransaction.commit()
     }
 }
